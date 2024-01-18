468b2fcca85e7b34de1fec2e4190692b0034fd8a
kien <kien3710@gmail.com>
2024-01-18 20:06:34 +0700

    push

---
+++ app/contact/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="contact py-[50px]">
+      <div className="container mx-auto text-white  text-[36px]">contact</div>
+    </div>
+  );
+};
+
+export default Contact;
